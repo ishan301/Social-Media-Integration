e5e6064c5dde247909747a34b3ee8e45ac76e0f4
ishan <2040129@sliet.ac.in>
2021-11-15 01:31:42 +0530

    google sign in working now

---
+++ app/src/main/java/com/example/socialmediaintegration/Dashboard.kt
@@ -0,0 +1,33 @@
+package com.example.socialmediaintegration
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_dashboard.*
+
+class Dashboard : AppCompatActivity() {
+
+    private lateinit var auth:FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+
+        auth= FirebaseAuth.getInstance()
+        val currentuser = auth.currentUser
+
+        name.text=currentuser?.displayName
+        email.text=currentuser?.email
+
+        Glide.with(this).load(currentuser?.photoUrl).into(profile_image)
+
+        sign_out_btn.setOnClickListener {
+            auth.signOut()
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
--- app/src/main/java/com/example/socialmediaintegration/SplashScreen.kt
+++ app/src/main/java/com/example/socialmediaintegration/SplashScreen.kt
@@ -4,18 +4,29 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
 
 class SplashScreen : AppCompatActivity() {
 
-    lateinit var handler: Handler
+    lateinit var auth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        handler = Handler()
-        handler.postDelayed({
-            val intent = Intent(this,MainActivity::class.java)
-            startActivity(intent)
+        auth= FirebaseAuth.getInstance()
+        val user=auth.currentUser
+
+        // If user is not authenticated send him to MainActivity(for signing in) else navigate him to dashboard
+        Handler().postDelayed({
+            if(user!=null)
+            {
+                val dashboardIntent=Intent(this,Dashboard::class.java)
+                startActivity(dashboardIntent)
+            }
+            else {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
             finish()
         },3000) //delay time
     }
